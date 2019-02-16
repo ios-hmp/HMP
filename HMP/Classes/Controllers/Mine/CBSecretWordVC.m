aa6bb16c9fde94714c2871f39380e0cc90d2bf28
hanzhanbing <1655661337@qq.com>
2019-02-16 14:54:46 +0800

    发密语UI搭建

--- HMP/Classes/Controllers/Mine/CBSecretWordVC.m
+++ HMP/Classes/Controllers/Mine/CBSecretWordVC.m
@@ -7,6 +7,7 @@
 //
 
 #import "CBSecretWordVC.h"
+#import "CBMessageVC.h"
 
 @interface CBSecretWordVC ()
 
@@ -14,9 +15,42 @@
 
 @implementation CBSecretWordVC
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    _titleView.top = 0;
+    _titleView.centerX = CB_SCREENWIDTH/2.0;
+    [self.navigationController.navigationBar addSubview:_titleView];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [_titleView removeFromSuperview];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.title = @"";
     // Do any additional setup after loading the view.
+}
+
+-(void)configUI{
+    UIBarButtonItem *item1 = [[UIBarButtonItem alloc]initWithCustomView:_navRightBtn];
+    self.navigationItem.rightBarButtonItem = item1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (self.titleLeftBtn.selected) {
+        MessageTabCell *cell = [tableView dequeueReusableCellWithIdentifier:@"screctTabCell1"];
+        return cell;
+    }
+    else {
+        SecretTabCell2 *cell = [tableView dequeueReusableCellWithIdentifier:@"SecretTabCell2"];
+        return cell;
+    }
 }
 
 /*
@@ -28,5 +62,22 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (IBAction)titleBtnAction:(UIButton *)sender {
+    __weak typeof(self) weakSelf = self;
+    sender.selected = YES;
+    [UIView animateWithDuration:0.2 animations:^{
+        weakSelf.titleLineView.centerX = sender.centerX;
+        if ([sender isEqual:weakSelf.titleLeftBtn]) {
+            weakSelf.titleRightBtn.selected = NO;
+        }
+        else {
+            weakSelf.titleLeftBtn.selected = NO;
+        }
+    }];
+    [self.tableview reloadData];
+}
+@end
+@implementation SecretTabCell2
 
 @end
